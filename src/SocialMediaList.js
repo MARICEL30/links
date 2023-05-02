a8084f8d9276a96677fcd8567ce041482e13b107
MARICEL30 <mariaceliadragan@gmail.com>
2023-05-02 22:42:40 +0100

    add component

---
+++ src/SocialMediaList.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import './App.css';
+import './App.js';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faGithub, faInstagram, faMedium, faTwitter } from '@fortawesome/free-brands-svg-icons'
+
+const instagramIcon = <FontAwesomeIcon icon={faInstagram} size="2x" color='#fff' />
+const githubIcon = <FontAwesomeIcon icon={faGithub} size="2x" color='#fff'/>
+const mediumIcon = <FontAwesomeIcon icon={faMedium} size="2x" color='#fff'/>
+const twitterIcon = <FontAwesomeIcon icon={faTwitter} size="2x" color='#fff'/>
+
+const socialMediaLinks = [
+    { id: 1, icon: instagramIcon, link: "https://www.instagram.com/maricel_weblab/"},
+    { id: 2, icon: githubIcon, link: "https://github.com/MARICEL30" },
+    { id: 3, icon: mediumIcon, link: "https://medium.com/@mariaceliadragan" },
+    { id: 4, icon: twitterIcon, link: "https://twitter.com/weblab_maricel" },
+
+    ]
+
+    
+export default function SocialMediaList() {
+   
+const socialMediaLinksIcons = socialMediaLinks.map(item =>
+    <a href={item.link} target="_blank" rel="noopener noreferrer" key={item.id}><p>{item.icon}</p></a>);
+
+ return (
+  <ul className="social-icons">
+  <li className='social-media-icon'>{socialMediaLinksIcons}</li>
+  </ul>
+ )};
+
